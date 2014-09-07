b978e70aed4a010eebc1763cc72ccec3b9552df8
Randy Kern <randy@teamkern.net>
2014-09-07 10:24:23 -0700

    Added support for more Hue APIs

--- hue.js
+++ hue.js
@@ -2,8 +2,51 @@ var http = require('http');
 var https = require('https');
 var util = require('util');
 
-function getHueIP(ret)
-{
+function hueRequest(bridgeIP, username, method, api, body, ret) {
+    var url = '/api';
+    if (username) {
+        url = url + '/' + username;
+    }
+    if (api) {
+        url = url + api;
+    }
+
+    var options = {
+        host: bridgeIP,
+        method: method,
+        path: url,
+    }
+
+    var chunks = ''
+    var request = http.request(options, function(response) {
+        response.setEncoding('utf8');
+
+        response.on('data', function(chunk) {
+            chunks = chunks + chunk
+        });
+
+        response.on('end', function() {
+            try {
+                ret(null, JSON.parse(chunks));
+            }
+            catch (e) {
+                ret(e);
+            }
+        });
+    });
+
+    request.on('error', function(e) {
+        ret(e);
+    });
+
+    if (body) {
+        request.write(JSON.stringify(body));
+    }
+
+    request.end();
+}
+
+function getHueIP(ret) {
     var options = {
         host: 'www.meethue.com',
         path: '/api/nupnp',
@@ -33,14 +76,7 @@ function getHueIP(ret)
     request.end();
 }
 
-function createUserInner(attempt, bridgeIP, devicetype, username, ret)
-{
-    var options = {
-        host: bridgeIP,
-        path: '/api',
-        method: 'POST',
-    }
-
+function createUser(bridgeIP, devicetype, username, timeout, ret) {
     var body = {
         devicetype: devicetype,
     }
@@ -49,47 +85,18 @@ function createUserInner(attempt, bridgeIP, devicetype, username, ret)
         body.username = username;
     }
 
-    var chunks = ''
-    var request = http.request(options, function(response) {
-        response.setEncoding('utf8');
+    var retry = function(err, attempt, result) {
+        console.log('Attempt ' + attempt.toString());
 
-        response.on('data', function(chunk) {
-            chunks = chunks + chunk
-        });
-
-        response.on('end', function() {
-            try {
-                ret(null, attempt, JSON.parse(chunks));
-            }
-            catch (e) {
-                ret(e);
-            }
-        });
-    });
-
-    request.on('error', function(e) {
-        ret(e);
-    });
-
-    request.write(JSON.stringify(body));
-
-    request.end();
-}
-
-function createUser(bridgeIP, devicetype, username, ret)
-{
-    var MAXATTEMPTS = 30;
-
-    function retry(err, attempt, result) {
         if (err) {
             ret(err);
         }
         else {
-            console.log(util.inspect(result[0], { showHiden: true, depth: null }));
+            console.log(util.inspect(result, { showHiden: true, depth: null }));
 
             if (result[0].error) {
                 if (result[0].error.type === 101) {
-                    if (attempt === MAXATTEMPTS) {
+                    if (attempt === timeout) {
                         ret(result[0].error);
                         return;
                     }
@@ -99,7 +106,9 @@ function createUser(bridgeIP, devicetype, username, ret)
                     }
 
                     setTimeout(function() {
-                        createUserInner(attempt + 1, bridgeIP, devicetype, username, retry);
+                        hueRequest(bridgeIP, null, 'POST', '', body, function(err, result) {
+                            retry(err, attempt + 1, result);
+                        });
 
                     }, 1000);
                 }
@@ -108,12 +117,30 @@ function createUser(bridgeIP, devicetype, username, ret)
                 }
             }
             else {
-                ret(null, result);
+                ret(null, result[0]);
             }
         }
     }
 
-    createUserInner(0, bridgeIP, devicetype, username, retry);
+    hueRequest(bridgeIP, null, 'POST', '', body, function(err, result) {
+        retry(err, 0, result);
+    });
+}
+
+function getConfiguration(bridgeIP, username, ret) {
+    hueRequest(bridgeIP, username, 'GET', '/config', null, ret);
+}
+
+function deleteUser(bridgeIP, username, usertodelete, ret) {
+    hueRequest(bridgeIP, username, 'DELETE', '/config/whitelist/' + usertodelete, null, ret);
+}
+
+function getFullState(bridgeIP, username, ret) {
+    hueRequest(bridgeIP, username, 'GET', '', null, ret);
+}
+
+function getLights(bridgeIP, username, ret) {
+    hueRequest(bridgeIP, username, 'GET', '/lights', null, ret);
 }
 
 
@@ -124,7 +151,12 @@ getHueIP(function(err, ip) {
         return;
     }
 
-    createUser(ip, 'tkhue', '3c3b79b41b2987ef10d80fdc396a5557', function(err, response) {
+    //createUser(ip, 'tkhue', '3c3b79b41b2987ef10d80fdc396a5557', 5,
+    //getConfiguration(ip, '3c3b79b41b2987ef10d80fdc396a5557',
+    //deleteUser(ip, '3c3b79b41b2987ef10d80fdc396a5557', 'ada49f56b77363244d19e44d6d22aa0b',
+    //getFullState(ip, '3c3b79b41b2987ef10d80fdc396a5557',
+    getLights(ip, '3c3b79b41b2987ef10d80fdc396a5557',
+        function(err, response) {
         if (err) {
             console.log('ERROR: ' + JSON.stringify(err));
         }
@@ -132,6 +164,7 @@ getHueIP(function(err, ip) {
             console.log(util.inspect(response, { showHiden: true, depth: null }));
         }
     });
+
 });
 
 
